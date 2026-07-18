5b8e33892e4ada53bb2edebae456b2de2998754f
BARRIOS <iml.health@pm.me>
2026-07-18 21:17:42 +0200

    Update Vite configuration with base and publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/",
+  publicDir: "public",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
 });
+
+
 
 
 
