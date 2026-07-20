11da61421e0bc1eddde889c85fab619fbf14a477
BARRIOS <iml.health@pm.me>
2026-07-20 13:09:45 +0200

    Remove './public/' from manuscript URLs

--- constants/documents.js
+++ constants/documents.js
@@ -1,5 +1,5 @@
 export const MANUSCRIPT_URL =
-  `${import.meta.env.BASE_URL}./public/IML_Founding_Manuscript.pdf`;
+  `${import.meta.env.BASE_URL}IML_Founding_Manuscript.pdf`;
 
 export const TECHNICAL_MANUSCRIPT_URL =
-  `${import.meta.env.BASE_URL}./public/IML_Technical_Manuscript.pdf`;
+  `${import.meta.env.BASE_URL}IML_Technical_Manuscript.pdf`;
