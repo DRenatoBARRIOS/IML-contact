9d468447c2e41a2a29a5bb8c06b63ca785cdae34
BARRIOS <iml.health@pm.me>
2026-07-20 13:00:32 +0200

    Fix manuscript URLs in documents.js
    Updated manuscript URLs to include './public/' path.

--- constants/documents.js
+++ constants/documents.js
@@ -3,3 +3,11 @@ export const MANUSCRIPT_URL =
 
 export const TECHNICAL_MANUSCRIPT_URL =
   `${import.meta.env.BASE_URL}IML_Technical_Manuscript.pdf`;
+
+
+
+export const MANUSCRIPT_URL =
+  `${import.meta.env.BASE_URL}./public/IML_Founding_Manuscript.pdf`;
+
+export const TECHNICAL_MANUSCRIPT_URL =
+  `${import.meta.env.BASE_URL}./public/IML_Technical_Manuscript.pdf`;
