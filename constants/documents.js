ac51e03bd88837aec11244fd94dc5a5f160714a2
BARRIOS <iml.health@pm.me>
2026-07-20 13:15:41 +0200

    Delete constants/documents.js

--- constants/documents.js
+++
@@ -1,5 +0,0 @@
-export const MANUSCRIPT_URL =
-  `${import.meta.env.BASE_URL}IML_Founding_Manuscript.pdf`;
-
-export const TECHNICAL_MANUSCRIPT_URL =
-  `${import.meta.env.BASE_URL}IML_Technical_Manuscript.pdf`;
