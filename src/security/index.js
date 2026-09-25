afa837a8334cad9ce41e2ab8c076542de4012a65
BARRIOS <iml.health@pm.me>
2026-09-25 16:00:14 +0200

    Export SECURITY-02 access controls

--- src/security/index.js
+++ src/security/index.js
@@ -11,3 +11,21 @@ export {
   TechnicalUnavailableError,
   executeAiAccessPlan,
 } from './aiBoundary.js';
+
+export {
+  ACCESS_MATRIX_VERSION,
+  CORE_ACCESS_RULES,
+  PURPOSE,
+  RESOURCE_TYPE,
+  ROLE_CODE,
+} from './accessMatrix.js';
+
+export {
+  BREAK_GLASS_MAX_MINUTES,
+  BREAK_GLASS_STEP_UP_MAX_AGE_MINUTES,
+  BreakGlassDeniedError,
+  validateBreakGlassRequest,
+  createBreakGlassGrant,
+  validateBreakGlassGrant,
+  buildBreakGlassReadRule,
+} from './breakGlass.js';
